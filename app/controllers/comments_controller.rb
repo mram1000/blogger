a8df0b40460736624a4daaa8b6d585a8c8e28f36
mram1000 <mram1000@gmail.com>
2014-08-17 18:14:38 +0000

    added blog comments

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,12 @@
+class CommentsController < ApplicationController
+  include CommentsHelper 
+def create
+  @comment = Comment.new(comment_params)
+  @comment.article_id = params[:article_id]
+  @comment.save
+  flash.notice = "Comment by '#{@comment.author_name}' created!"
+  redirect_to article_path(@comment.article)
+end
+  
+  
+end
